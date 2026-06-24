0f41df818a58de1398209aac05511eb74feb2eb6
nibanovic <nikola.banovic@b-robotized.com>
2026-06-24 18:07:44 +0200

    TEMP: assume mode is supported

--- canopen_402_driver/src/motor.cpp
+++ canopen_402_driver/src/motor.cpp
@@ -47,8 +47,17 @@ uint16_t Motor402::getMode()
 
 bool Motor402::isModeSupportedByDevice(uint16_t mode)
 {
-  uint32_t supported_modes =
-    driver->universal_get_value<uint32_t>(supported_drive_modes_index, 0x0);
+  uint32_t supported_modes = 0;
+  try 
+  {
+    supported_modes = driver->universal_get_value<uint32_t>(supported_drive_modes_index, 0x0);
+  }
+  catch (...) 
+  {
+    RCLCPP_WARN(rclcpp::get_logger("canopen_402_driver"), 
+                "Failed to read supported_drive_modes (0x6502). Assuming mode %d is supported.", mode);
+    return true; 
+  }
   bool supported = supported_modes & (1 << (mode - 1));
   bool below_max = mode <= 32;
   bool above_min = mode > 0;
